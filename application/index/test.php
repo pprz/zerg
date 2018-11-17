dca44c280ae57be4ddfa97873d79e96d2b776ac9
nxinran <334866400@qq.com>
2018-11-18 00:36:43 +0800

    create zerg

---
+++ application/index/test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/11/12
+ * Time: 21:57
+ */
+
+class test
+{
+
+}
---
+++ public/info.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/11/17
+ * Time: 21:19
+ */
+
+phpinfo();
